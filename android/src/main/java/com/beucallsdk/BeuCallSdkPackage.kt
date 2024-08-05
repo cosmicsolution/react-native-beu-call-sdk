46f7d6b16cc6031646a283deeb7cdc1425791d4a
Vu Phan <ptvu.it@gmail.com>
2024-08-06 01:23:37 +0700

    chore: initial commit

---
+++ android/src/main/java/com/beucallsdk/BeuCallSdkPackage.kt
@@ -0,0 +1,17 @@
+package com.beucallsdk
+
+import com.facebook.react.ReactPackage
+import com.facebook.react.bridge.NativeModule
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.uimanager.ViewManager
+
+
+class BeuCallSdkPackage : ReactPackage {
+  override fun createNativeModules(reactContext: ReactApplicationContext): List<NativeModule> {
+    return listOf(BeuCallSdkModule(reactContext))
+  }
+
+  override fun createViewManagers(reactContext: ReactApplicationContext): List<ViewManager<*, *>> {
+    return emptyList()
+  }
+}
---
+++ example/ios/File.swift
@@ -0,0 +1,6 @@
+//
+//  File.swift
+//  BeuCallSdkExample
+//
+
+import Foundation
